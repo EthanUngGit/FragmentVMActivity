e6f31a2e3996f031a08616a8021cce90f388ed52
EthanUngGit <ethan.ung@temple.edu>
2024-03-21 12:04:54 -0400

    Implemented DiceViewModel

--- app/src/main/java/edu/temple/diceroll/DiceFragment.kt
+++ app/src/main/java/edu/temple/diceroll/DiceFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlin.random.Random
 
 const val DIE_SIDES = "dIcE_SiDeS"
 
 class DiceFragment : Fragment() {
-    private var sides: Int? = null
 
     // ViewModel declaration
     private lateinit var viewModel: DiceViewModel
+    private var sides = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +36,20 @@ class DiceFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_dice, container, false).apply {
-            findViewById<Button>(R.id.rollButton).setOnClickListener {
-
-                // Generate random number
-                (Random.nextInt(sides!!) + 1).toString()
+        val view = inflater.inflate(R.layout.fragment_dice, container, false)
+            view.findViewById<Button>(R.id.rollButton).setOnClickListener {
+                sides?.let { sides ->
+                    viewModel.rollDice(sides)
+                }
             }
+
+            viewModel.diceRollResult.observe(viewLifecycleOwner, Observer { result ->
+                activity?.findViewById<TextView>(R.id.numberDisplay)?.text = result.toString()
+            })
+
+            return view
         }
-    }
+
 
     companion object {
 
